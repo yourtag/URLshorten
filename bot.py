140fe0bfdd793cdf6d7687ada34d6fda31f8a9ef
Jakeedot <86273620+Jakeedot@users.noreply.github.com>
2022-06-28 23:31:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,8 +66,8 @@ def echo(update, context):
         req = requests.get(f'https://shrinkearn.com/api?api={gptoken}&url={url_convert}')
         r = json.loads(req.content)
 
-        if r['status'] == 'link' :
-            update.message.reply_text(' Here is your ' + r['status'] + ' 👇')
+        if r['status'] == 'success' :
+            update.message.reply_text(' Here is your link ' + ' 👇')
             update.message.reply_text(' 🔗Shortened Url : ' + r['shortenedUrl'])
         if r['status'] == 'error':
             update.message.reply_text(' Error : ' + r['message'] + ' 👎')
