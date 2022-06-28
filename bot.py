1e0ea31efe24e4c9d3de0d129450777e515ebf2f
Jakeedot <86273620+Jakeedot@users.noreply.github.com>
2022-06-28 14:21:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,8 +23,15 @@ def start(update, context):
 
 
 def help_command(update, context):
+     keyboard = [
+       [
+            InlineKeyboardButton("🧐 How to use me", url='https://telegra.ph/How-to-use-me-10-29'),
+       ]
+    ]
 
-    update.message.reply_text("Hello, \n*Follow these steps:*\n\n🎈 First you have to sign up at tiny.one/GPlinks\n\n🎈 After that copy that link from GPLINK TOOLS API\n\n🎈 It will look like this 👉 `https://gplinks.in/api?api=6a4cb74d70edd33a&`\nsent it to me\n\n🎈 Now you are done! just sent any link to me")
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    update.message.reply_text('Hello, \n*Follow these steps:*\n\n🎈 First you have to sign up at tiny.one/GPlinks\n\n🎈 After that copy that link from GPLINK TOOLS API\n\n🎈 It will look like this 👉 `https://gplinks.in/api?api=6a4cb74d70edd33a&`\nsent it to me\n\n🎈 Now you are done! just sent any link to me', reply_markup=reply_markup)
     
 def auth(update, context): 
     keyboard = [
