306a981e374265451270d8946432585439bbe26e
Jakeedot <86273620+Jakeedot@users.noreply.github.com>
2022-06-28 11:42:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,8 +24,7 @@ def start(update, context):
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text(
-        'Hi! {update.message.from_user.first_name}\n\nIm GPlink bot. Just send me link and get short link\n\n/help for more help\n\nyou have to autherise me to use this bot use /auth\nyour api token is safe with me I will not share it \n\nany doubt ask here 👉 @Jakeedot\n\n©️ ', reply_markup=reply_markup)
+    update.message.reply_text('Hi! {}\n\nIm GPlink bot. Just send me link and get short link\n\n/help for more help\n\nyou have to autherise me to use this bot use /auth\nyour api token is safe with me I will not share it \n\nany doubt ask here 👉 @Jakeedot\n\n©️ ', reply_markup=reply_markup)
 
 
 def help_command(update, context):
@@ -54,7 +53,7 @@ def echo(update, context):
         tokens[chat] = str(token)
         with open('gplink_tokens.py', 'w') as file:
             file.write('tokens = ' + str(tokens))
-            update.message.reply_text(f'🎉 congratulations \n\nYour 😇 CHAT_ID : {chat} IS REGISTERED WITH GPLINK API TOKEN : {token}\n\nIf you sent me a different API URL I will reassign your GPLINK API TOKEN')
+            update.message.reply_text(f'🎉 Congratulations {firstname}\n\nYou are registered with GPLINK API TOKEN : {token}\n\nIf you sent me a different API URL I will reassign your GPLINK API TOKEN')
     elif 'https://gplinks.in/api?api=' not in str(update.message.text) and (re.search('^http://.*', str(update.message.text)) or re.search('^https://.*', str(update.message.text))):
         try:
             chat = str(update.message.chat_id)
